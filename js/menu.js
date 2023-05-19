bea8838b72f1b5409ba971b31f2cb9ff03c280cd
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-05-18 21:31:38 -0600

    [ADD] js logic change logo

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,18 @@
-const burguerIcon = document.querySelectorAll('.nav__icon');
+const burguerIcon = document.querySelector('.nav__icon');
+const logo = document.querySelector('.nav__logo');
 const menu = document.querySelector('.nav__list');
+let open = true;
 
-burguerIcon.forEach(element => {
-  element.addEventListener('click', () => {
-    console.log('Holla mundo')
-    
-    // element.classList.toggle();
+burguerIcon.addEventListener('click', ()=> {
 
-    menu.classList.toggle('nav__list--show');
-  })
+  if(open) {
+    logo.src= '/assets/images/mindfully_white.svg'
+    burguerIcon.src= '/assets/icons/icons8-close.svg';
+    open = false;
+  } else {
+    logo.src = "/assets/images/mindfully_logo.svg";
+    burguerIcon.src= '/assets/icons/icons8-menu.svg';
+    open = true;
+  }
+  menu.classList.toggle('nav__list--show');
 })
