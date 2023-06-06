05fd498daa4564102d88cb6ad44567001fb0f137
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-06-05 21:37:31 -0600

    [ADD]

--- js/main.js
+++ js/main.js
@@ -1,21 +1,8 @@
 const burguerIcon = document.querySelector('.nav__icon');
 const logo = document.querySelector('.nav__logo');
 const menu = document.querySelector('.nav__list');
-let open = true;
+let open = false;
 
-// burguerIcon.addEventListener('click', ()=> {
-
-//   if(open) {
-//     logo.src= '/assets/images/mindfully_white.svg'
-//     burguerIcon.src= '/assets/icons/icons8-close.svg';
-//     open = false;
-//   } else {
-//     logo.src = "/assets/images/mindfully_logo.svg";
-//     burguerIcon.src= '/assets/icons/icons8-menu.svg';
-//     open = true;
-//   }
-//   menu.classList.toggle('nav__list--show');
-// })
 
 const handleMenutoggle = () => {
   if(open) {
@@ -28,20 +15,20 @@ const handleMenutoggle = () => {
     open = true;
   }
   menu.classList.toggle('nav__list--show');
-
 }
 
 burguerIcon.addEventListener('click', () => {
-  if(window.innerWidth < 768) {
+  if(window.innerWidth < 768 ) {
     handleMenutoggle();
   }
 });
 
 window.addEventListener('resize', () => {
   if(window.innerWidth >= 768) {
-   menu.classList.add('nav__list--show');
+    open = false;
+    menu.classList.add('nav__list--show');
   } else {
     menu.classList.remove('nav__list--show');
-    
   }
 })
+
