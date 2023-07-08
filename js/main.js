f890a7e269f621147fa2613141f25d1e1af20165
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-07-07 22:09:44 -0600

    [Fix] menu classes

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 const burguerIcon = document.querySelector('.nav__icon');
 const logo = document.querySelector('.nav__logo');
 const menu = document.querySelector('.nav__list');
-let openMenu = true;
+ let menuOpen = false;
+
+
 
 const handleMenuToggle = () => {
-  if(openMenu) {
+  if(menuOpen) {
     logo.src= '/assets/images/mindfully_white.svg';
     burguerIcon.src = '/assets/icons/icons8-close.svg ';
-    openMenu = false;
+    menuOpen = false;
   } else {
     logo.src = "/assets/images/mindfully_logo.svg";
     burguerIcon.src= '/assets/icons/icons8-menu.svg';
-    openMenu = true;
+    menuOpen = true;
   }
-
-  menu.classList.toggle('nav__list--show');
-
+  
+  if(window.innerWidth >= 768){
+    menu.classList.toggle('nav__list--show');
+   }
 }
 
- burguerIcon.addEventListener('click', () => {
 
-  handleMenuToggle();
+burguerIcon.addEventListener('click', () => {
 
+      handleMenuToggle();
  })
-
