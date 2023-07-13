d0b1ee7ba0c4cd524ffd2b965d7f4e3f0b25b296
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2023-07-12 20:46:06 -0600

    [ADD] testimonials cards object fit cover

--- js/main.js
+++ js/main.js
@@ -2,23 +2,6 @@ const burguerIcon = document.querySelector('.nav__icon');
 const menu = document.querySelector('.nav__container');
  let menuOpen = false;
 
-// const handleMenuToggle = () => {
-//   if(menuOpen) {
-//     logo.src= '/assets/images/mindfully_white.svg';
-//     burguerIcon.src = '/assets/icons/icons8-close.svg ';
-//     menuOpen = false;
-//   } else {
-//     logo.src = "/assets/images/mindfully_logo.svg";
-//     burguerIcon.src= '/assets/icons/icons8-menu.svg';
-//     menuOpen = true;
-//   }
-  
-//   if(window.innerWidth >= 768){
-//     menu.classList.toggle('nav__list--show');
-//    }
-// }
-
-
 burguerIcon.addEventListener('click', () => {
 
   const logo = document.querySelector('.nav__logo');
